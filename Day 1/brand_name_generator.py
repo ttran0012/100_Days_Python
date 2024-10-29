a57c2121bea67be95706731ade5eb178ed4afe01
Tan Tran <trantan0012@gmail.com>
2024-10-28 21:37:34 -0400

    Re-adding Day 1 Project

---
+++ Day 1/brand_name_generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Brand Name Generator")
+
+get_input = input("What's the name of the city you grew up in?")
+get_pet_name = input("What's your pet's name?")
+
+print("Your Brand Name could be " + get_input + " " + get_pet_name)
---
+++ Day 1/dayone.py
@@ -0,0 +1,9 @@
+name = input("what is your name?")
+
+print(len(name))
+
+# swap
+a = "juice"
+b = "milk"
+a , b = b, a # this is python way of easy swap
+print(a, b)
