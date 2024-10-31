a23bd619ddfad7c84189286499281b8b63ebdd72
Tan Tran <trantan0012@gmail.com>
2024-10-30 21:28:14 -0400

    Add not on nested list

--- Day 4/day_four_note.py
+++ Day 4/day_four_note.py
@@ -5,16 +5,31 @@
 
 
 # List
-fruits = [item1, item2]
-fruits = ["cherry", "test", "test2"]
-            [0]      [1]       [2]
+#fruits = [item1, item2]
+#fruits = ["cherry", "test", "test2"]
+#            [0]      [1]       [2]
 # To access a list you would need to access its index
-fruits[0] # this will return Cherry
-[-3] # this will return list from the "Right" side
+#fruits[0] # this will return Cherry
+#[-3] # this will return list from the "Right" side
 
 # To change an item in the list
 
-fruits[1] = "New CHanges" # This will replace the index with the updated
+#fruits[1] = "New CHanges" # This will replace the index with the updated
 
-.append # to add an item to the end of the list
-.extend (["add item", "add item"]) # this will extend 2 item at the end of the list
+#.append # to add an item to the end of the list
+#.extend (["add item", "add item"]) # this will extend 2 item at the end of the list
+
+#fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+#print(fruits[-5])
+
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+#print(dirty_dozen)
+#print(dirty_dozen[1][1])
+print(dirty_dozen[0])
+print(dirty_dozen[1])
+print(dirty_dozen[1][2])
+print(dirty_dozen[1][3])
