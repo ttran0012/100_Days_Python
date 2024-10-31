8a7115d7d67649c6ad62a27c551f70c8ea805ce0
Tan Tran <trantan0012@gmail.com>
2024-10-30 21:19:46 -0400

    Add who_will_pay.py using random and len() to return randomization

---
+++ Day 4/day_four_note.py
@@ -0,0 +1,20 @@
+# python Randomization
+# import random
+# random()
+# random.randint(a:int,b:int)
+
+
+# List
+fruits = [item1, item2]
+fruits = ["cherry", "test", "test2"]
+            [0]      [1]       [2]
+# To access a list you would need to access its index
+fruits[0] # this will return Cherry
+[-3] # this will return list from the "Right" side
+
+# To change an item in the list
+
+fruits[1] = "New CHanges" # This will replace the index with the updated
+
+.append # to add an item to the end of the list
+.extend (["add item", "add item"]) # this will extend 2 item at the end of the list
---
+++ Day 4/who_will_pay.py
@@ -0,0 +1,7 @@
+import random
+
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+random_index = random.randrange(len(friends))
+
+print(friends[random_index])
