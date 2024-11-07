80e4ba7681cb6c85ca61b54175f9bca95c257ba8
Tan Tran <trantan0012@gmail.com>
2024-11-06 19:54:46 -0500

    add FizzBuzz

--- Day 5/fizzbuzz.py
+++ Day 5/fizzbuzz.py
@@ -1,8 +1,8 @@
-from schedule import clear
 
-for i in range(1,100):
+n = 100
 
-    if i % 3 == 0 and i % 5 == 0:
+for i in range(1, n+1):
+    if i % 3 == 0 and  i % 5 == 0:
         print("FizzBuzz")
     elif i % 3 == 0:
         print("Fizz")
